03349ecf37cbea4710915ed7bbfacdad88a995fb
Melady1980 <carol.d.hernandez@outlook.com>
2022-10-13 16:06:15 +0000

    Function to print number pnum, and function to convert to char.

--- functions.c
+++ functions.c
@@ -16,7 +16,6 @@ int _putchar(char c)
  * @s: string to be printed
  * Return: numbers of characters printed
  */
-
 int _puts(char *s)
 {
 	int count = 0;
@@ -27,4 +26,50 @@ int _puts(char *s)
 		s++;
 	}
 		return (count);
+}
+
+/**
+ * convert - Function that converts number to base
+ * @num: input number
+ * @base: input base
+ * Return: pointer to string
+ */
+char *convert(int num, int base)
+{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
+
+	rep = "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+	return (ptr);
+}
+
+/**
+ * _pnum - Function to prints number
+ * @num: number to be printed
+ * Return: counter to print
+ */
+int _pnum(int num)
+{
+	int count = 0;
+	char *s;
+
+	if (num < 0)
+	{
+		num = -num;
+		count += _putchar('-');
+	}
+	s = convert(num, 10);
+	while (*s != '\0')
+	{
+		count += _putchar(*s);
+		s++;
+	}
+	return (count);
 }
